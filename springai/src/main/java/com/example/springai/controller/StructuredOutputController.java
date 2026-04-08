73dbece010e41516d7ab24e8ed3a4e9d2c09d21b
Itsjeet007y1 <jeet.62.jp@gmail.com>
2026-04-08 22:39:45 +0530

    Structured output as List and Map

--- springai/src/main/java/com/example/springai/controller/StructuredOutputController.java
+++ springai/src/main/java/com/example/springai/controller/StructuredOutputController.java
@@ -1,14 +1,18 @@
 package com.example.springai.controller;
 
-import com.example.springai.advisors.TokenUsageAuditAdvisor;
 import com.example.springai.model.CountryCities;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
+import org.springframework.ai.converter.ListOutputConverter;
+import org.springframework.ai.converter.MapOutputConverter;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api")
@@ -28,5 +32,25 @@ public class StructuredOutputController {
                 .user(message)
                 .call().entity(CountryCities.class);
         return ResponseEntity.ok(countryCities);
+    }
+
+    @GetMapping("/chat-list")
+    public ResponseEntity<List<String>> chatList(@RequestParam String message) {
+        List<String> list = chatClient
+                .prompt()
+                .advisors(new SimpleLoggerAdvisor())
+                .user(message)
+                .call().entity(new ListOutputConverter());
+        return ResponseEntity.ok(list);
+    }
+
+    @GetMapping("/chat-map")
+    public ResponseEntity<Map<String, Object>> chatMap(@RequestParam String message) {
+        Map<String, Object> list = chatClient
+                .prompt()
+                .advisors(new SimpleLoggerAdvisor())
+                .user(message)
+                .call().entity(new MapOutputConverter());
+        return ResponseEntity.ok(list);
     }
 }
