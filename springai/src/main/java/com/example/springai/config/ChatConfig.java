3cb501022b39f70f63c031f0fb6aefdabb7b1e00
Itsjeet007y1 <jeet.62.jp@gmail.com>
2026-04-08 09:02:02 +0530

    Configured basics chat options

--- springai/src/main/java/com/example/springai/config/ChatConfig.java
+++ springai/src/main/java/com/example/springai/config/ChatConfig.java
@@ -2,6 +2,7 @@ package com.example.springai.config;
 
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
+import org.springframework.ai.chat.prompt.ChatOptions;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,8 +11,14 @@ public class ChatConfig {
 
     @Bean
     public ChatClient chatClient(ChatClient.Builder chatClientBuilder) {
+        ChatOptions chatOptions = ChatOptions.builder().model("gpt-3.5-turbo")
+                .maxTokens(10)
+                .temperature(0.7)
+                .build();
+
         return chatClientBuilder
                 .defaultAdvisors(new SimpleLoggerAdvisor())
+                .defaultOptions(chatOptions)
                 .defaultSystem("You are an internal HR assistant. Your role is to help" +
                         " employees with questions related to HR policies, such as " +
                         "leave policies, working hours, benefits, and other HR-related inquiries. " +
