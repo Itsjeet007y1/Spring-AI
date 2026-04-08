1860f8b25980910effa04ce5beb34db7f714e5c6
Itsjeet007y1 <jeet.62.jp@gmail.com>
2026-04-08 21:31:50 +0530

    Options in prompt Stuffing

--- springai/src/main/java/com/example/springai/controller/PromptStuffingController.java
+++ springai/src/main/java/com/example/springai/controller/PromptStuffingController.java
@@ -1,6 +1,7 @@
 package com.example.springai.controller;
 
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.openai.OpenAiChatOptions;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.Resource;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -25,6 +26,11 @@ public class PromptStuffingController {
     public String chat(@RequestParam("message") String message) {
         return chatClient
                 .prompt()
+                .options(OpenAiChatOptions.builder()
+                        .model("gpt-3.5-turbo")
+                        .maxTokens(10)
+                        .temperature(0.7)
+                        .build())
                 .system(systemPromptTemplate)
                 .user(message)
                 .call().content();
